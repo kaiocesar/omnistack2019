6d59e63351663699b4e386cfd78717f1dd504585
Kaio Cesar <tecnico.kaio@gmail.com>
2019-10-12 21:59:37 -0300

    html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css';
 
+import logo from "./assets/logo.svg"
+
 function App() {
   return (
-    <h1>Testing...</h1>
+    <div className="container">
+      <img src="" alt=""/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa.
+        </p>
+        <form action="">
+          <label htmlFor="">E-mail</label>
+          <input type="email" id="email"/>
+        </form>
+      </div>
+    </div>
   );
 }
 
